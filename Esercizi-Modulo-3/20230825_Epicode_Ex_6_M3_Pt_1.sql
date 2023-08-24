ca203da52e11bb6d345495c53c30ea6eff207fcb
MarcoFer23 <marco.fer.213@gmail.com>
2023-08-24 23:55:49 +0200

    upload drafts ex 6 m3 pt 1 & 2

---
+++ Esercizi-Modulo-3/20230825_Epicode_Ex_6_M3_Pt_1.sql
@@ -0,0 +1,38 @@
+create database esercizio_6_m3_pt_1;
+
+use esercizio_6_m3_pt_1;
+
+create table Studente (
+    matricola_studente varchar(255),
+    nome_studente varchar(255),
+    città_studente varchar(255),
+        primary key (matricola_studente)
+);
+
+create table Docente (
+    matricola_docente varchar(255),
+    nome_docente varchar(255),
+        primary key (matricola_docente,nome_docente)
+);
+
+create table Corso (
+    codice_corso varchar(255),
+    nome_corso varchar(255),
+    matricola_docente varchar(255),
+primary key (codice_corso),
+constraint fk_matricola_docente_Corso_Docente foreign key (matricola_docente) references Docente(matricola_docente) on update cascade on delete no action
+);
+
+create table Esame (
+    codice_corso varchar(255),
+    matricola_studente varchar(255),
+    data_esame date,
+    voto_esame tinyint unsigned,
+    settore_scientifico varchar(255),
+    primary key (codice_corso,matricola_studente),
+    constraint fk_codice_corso_Esame_Corso foreign key (codice_corso) references Corso(codice_corso) on update cascade on delete no action,
+    constraint fk_matricola_studente_Esame_Studente foreign key (matricola_studente) references Studente(matricola_studente) on update cascade on delete no action
+);
+
+
+
---
+++ Esercizi-Modulo-3/20230825_Epicode_Ex_6_M3_Pt_2.sql
@@ -0,0 +1,4 @@
+create database esercizio_6_m3_pt_2;
+
+use esercizio_6_m3_pt_2;
+
