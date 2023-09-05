6e0fd73c8f2e3bca7a3e228ce33d242d3895b81b
MarcoFer23 <marco.fer.213@gmail.com>
2023-09-05 21:27:24 +0200

    Upld Ex 1 M4 "Python"

---
+++ Esercizi-Modulo-4/20230905_Epicode_Ex_1_M4.py
@@ -0,0 +1,49 @@
+'''
+1. Abbiamo 25 studenti; memorizzare questo dato in una variabile.
+'''
+studenti = 25
+
+'''
+2. Abbiamo 25 studenti; memorizzare questo dato in una variabile e stamparla a video.
+'''
+studenti = 25
+print("Studenti: ",studenti)
+
+'''
+3. Abbiamo 25 studenti; memorizzare questo dato in una variabile. 
+    Arrivano altri 3 studenti; memorizzare questo dato in un'altra variabile.
+'''
+studenti = 25
+altri_studenti = 3
+
+'''
+4. Abbiamo 25 studenti; memorizzare questo dato in una variabile. 
+    Arrivano altri 3 studenti; memorizzare questo dato in un'altra variabile. 
+        Creare un'altra variabile ancora che contenga la somma delle prime due, poi stamparla a video.
+'''
+studenti = 25
+altri_studenti = 3 
+somma_studenti = studenti + altri_studenti
+print(somma_studenti)
+
+'''
+5. Creare, a mano, una lista che contenga i numeri da 0 a 5, memorizzarla in una variable e infine stamparla a video.
+'''
+lista_numeri = [0,1,2,3,4,5]
+print("La lista è questa: ",lista_numeri)
+
+# alternativa con "range"
+
+lista_range = range(0,6)
+print("La lista range è questa: ",list(lista_range))
+
+'''
+6. Crea, a mano, una lista che contenga tre dei tuoi film preferiti, memorizzala in una variabile e infine stampala a video.
+'''
+lista_film = [
+    "The Lord of the Rings",
+    "The Nightmare Before Christmas",
+    "Blade Runner"
+]
+print(lista_film)
+
