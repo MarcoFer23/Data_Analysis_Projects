5318c94f3e5a4437dffc7ffa7846f4355921cb51
Marco <marco.fer.213@gmail.com>
2026-04-29 00:14:15 +0200

    sql exercises review

---
+++ SQL_Projects/20230804_01_Create_Database_Table.sql
@@ -0,0 +1,70 @@
+/* One basic exercise to practice database and table creation.
+Let's create a videogame store databse, and fill it with dedicated tables: */
+
+
+CREATE DATABASE videogames;
+
+USE videogames;
+
+CREATE TABLE Store (
+    store_id INT AUTO_INCREMENT,
+    store_address VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    PRIMARY KEY (store_id)
+);
+
+CREATE TABLE Employee (
+    tax_id_num VARCHAR(16),
+    full_name VARCHAR(255) NOT NULL,
+    degree VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    job VARCHAR(255) NOT NULL,
+    store_id INT NOT NULL,
+    hire_date DATE NOT NULL,
+    employment_termination_date DATE,
+    PRIMARY KEY (tax_id_num),
+    CONSTRAINT FK_store_employee FOREIGN KEY (store_id)
+        REFERENCES Store (store_id)
+        ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+CREATE TABLE Department (
+    store_id INT,
+    department_id INT,
+    PRIMARY KEY (store_id , department_id),
+    CONSTRAINT FK_store_department FOREIGN KEY (store_id)
+        REFERENCES Store (store_id)
+        ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+CREATE TABLE Videogames (
+    game_id INT AUTO_INCREMENT,
+    title VARCHAR(255),
+    developer VARCHAR(255),
+    release_year INT NOT NULL,
+    price DECIMAL(5 , 2 ) NOT NULL,
+    genre VARCHAR(255) NOT NULL,
+    remake BOOLEAN NOT NULL,
+    PRIMARY KEY (game_id)
+);
+
+
+CREATE TABLE Location (
+    store_id INT,
+    game_id INT,
+    department_id INT,
+    position VARCHAR(255) NOT NULL,
+    available BOOLEAN NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    developer VARCHAR(255) NOT NULL,
+    PRIMARY KEY (store_id , department_id , title , developer),
+    CONSTRAINT FK_store_location FOREIGN KEY (store_id)
+        REFERENCES Store (store_id)
+        ON UPDATE CASCADE ON DELETE NO ACTION,
+    CONSTRAINT FK_store_department_location FOREIGN KEY (store_id , department_id)
+        REFERENCES Department (store_id , department_id)
+        ON UPDATE CASCADE ON DELETE NO ACTION,
+    CONSTRAINT FK_videogame_id FOREIGN KEY (game_id)
+        REFERENCES Videogames (game_id)
+        ON UPDATE CASCADE ON DELETE NO ACTION
+);
---
+++ SQL_Projects/20230804_02_Create_Database_Table_&_Basic_Queries.sql
@@ -0,0 +1,80 @@
+/* One basic exercise to practice database table creation, and basic SQL queries.
+Let's create an airport database, and fill it with dedicated tables: */
+
+
+CREATE DATABASE airport_data;
+
+USE airport_data;
+
+
+CREATE TABLE airports (
+    iata_code CHAR(3) NOT NULL,
+    airport_name VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    runways TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY (iata_code),
+    CONSTRAINT chk_iata_upper CHECK (iata_code = UPPER(iata_code)),
+    CONSTRAINT chk_runways CHECK (runways > 0)
+);
+
+CREATE TABLE airplanes (
+    airplane_id INT NOT NULL AUTO_INCREMENT,
+    airplane_type VARCHAR(255) NOT NULL,
+    passenger_capacity SMALLINT UNSIGNED NOT NULL,
+    goods_volume INT UNSIGNED NOT NULL,
+    PRIMARY KEY (airplane_id)
+);
+
+CREATE TABLE flights (
+    flight_id INT NOT NULL AUTO_INCREMENT,
+    airplane_id INT NOT NULL,
+    departure_airport CHAR(3) NOT NULL,
+    departure_time TIMESTAMP NOT NULL,
+    destination_airport CHAR(3) NOT NULL,
+    landing_time TIMESTAMP NOT NULL,
+    PRIMARY KEY (flight_id),
+    FOREIGN KEY (airplane_id)
+        REFERENCES airplanes (airplane_id)
+        ON UPDATE CASCADE ON DELETE NO ACTION,
+    FOREIGN KEY (departure_airport)
+        REFERENCES airports (iata_code)
+        ON UPDATE CASCADE ON DELETE NO ACTION,
+    FOREIGN KEY (destination_airport)
+        REFERENCES airports (iata_code)
+        ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+CREATE INDEX idx_route ON flights(departure_airport, destination_airport);
+CREATE INDEX idx_departure_time ON flights(departure_time);
+CREATE INDEX idx_landing_time ON flights(landing_time);
+
+-- Let's search for airports with a total number of runways between 2 and 5:
+
+SELECT iata_code, airport_name, city
+FROM airports
+WHERE runways BETWEEN 2 and 5;
+
+-- Search for flights arriving at Keflavik (Reykjavik) airport:
+
+SELECT flight_id, airplane_id, departure_airport, departure_time
+FROM flights
+WHERE destination_airport = 'KEF';
+
+-- Search for flights arriving at an airport whose Iata Code starts with K:
+
+SELECT flight_id, airplane_id, departure_airport, departure_time
+FROM flights
+WHERE destination_airport LIKE 'K%';
+
+-- Search for flights taking off from Keflavik (Reykjavik) airport:
+
+SELECT flight_id, airplane_id
+FROM flights
+WHERE departure_airport = 'KEF';
+
+-- Search for flights having both departure and destination airports following specific patterns:
+
+SELECT flight_id, airplane_id, departure_time
+FROM flights
+WHERE departure_airport LIKE 'J%' AND destination_airport LIKE 'K%';
